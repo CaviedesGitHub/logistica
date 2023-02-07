4a58d7c2db4de1280d4135c775ec3eb592cc1eb3
CaviedesGitHub <l.padillac2@uniandes.edu.co>
2023-02-06 22:31:58 -0500

    docker

--- offers_ms/app.py
+++ offers_ms/app.py
@@ -1,11 +1,12 @@
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+import os
 
 from flask import Flask
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost:5432/OffersDB'  
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("POSTGRES_URL") #'postgresql://admin:admin@localhost:5432/OffersDB'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'nativa2023'
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = True
--- offers_ms/run.sh
+++ offers_ms/run.sh
@@ -3,5 +3,5 @@ export FLASK_DEBUG=1
 export FLASK_ENV=development
 
 sudo ufw allow 5003
-gunicorn --bind 0.0.0.0:5003 wsgi:app.py
+gunicorn --bind 0.0.0.0:5003 wsgi:app
 
